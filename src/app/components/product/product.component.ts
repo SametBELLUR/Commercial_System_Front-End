5ea0e9d50b653a509d6520b4904a856214b071d2
SametBELLUR <73693190+SametBELLUR@users.noreply.github.com>
2021-03-17 20:21:33 +0300

    Week: 9 Day: 1

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,51 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from 'src/app/models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  product1: any = {
-    productId: 1,
-    productName: 'Bardak',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product2: any = {
-    productId: 2,
-    productName: 'Laptop',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product3: any = {
-    productId: 3,
-    productName: 'Mause',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product4: any = {
-    productId: 4,
-    productName: 'Keyboard',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product5: any = {
-    productId: 5,
-    productName: 'Camera',
-    categoryId: 1,
-    unitPrice: 5,
-  };
+  products: Product[] = [];
+  dataLoaded = false;
+  constructor(private productService: ProductService) {}
 
-  products: any = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  getProducts() {
+    this.productService
+      .getProducts()
+      .subscribe(response =>{
+        this.products = response.data
+        this.dataLoaded = true;
+      })
+  }
 }
