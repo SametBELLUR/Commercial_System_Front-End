24919ef2538fab0a93f8bb61bf45bc8b4564b0fd
SametBELLUR <sametbellur@gmail.com>
2021-03-24 01:53:33 +0300

    Week: 11 Day: 1

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ToastrService } from 'ngx-toastr';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -22,5 +24,12 @@ export class ProductService {
     let newPath =
       this.apiUrl + 'products/getbycategory?categoryId=' + categoryId;
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  add(product: Product): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(
+      this.apiUrl + 'products/add',
+      product
+    );
   }
 }
